87eb0a96552055aed3e6b28850c0ee3313b10aa7
Luca Carlon <carlon.luca@gmail.com>
2022-09-17 02:18:26 +0200

    Implemented request for proc data.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello 2")
+import pidusage = require("pidusage")
+
+pidusage(1758, { "usePs": true }, (err, stats: pidusage.Status) => {
+    if (err) {
+        console.warn("Error:", err.message)
+        return
+    }
+
+    console.log("Stats:", stats.cpu)
+})
