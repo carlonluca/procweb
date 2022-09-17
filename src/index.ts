263f601b2de1b87b3ba79d29fa527d88d9181c90
Luca Carlon <carlon.luca@gmail.com>
2022-09-17 17:21:02 +0200

    Read pid from command line.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import pidusage = require("pidusage")
+import yargs = require("yargs")
+import helpers = require('yargs/helpers')
 
-pidusage(1758, { "usePs": true }, (err, stats: pidusage.Status) => {
+let argv = yargs(helpers.hideBin(process.argv))
+    .command("<pid>", "Monitor a process", (yargs) => {
+        yargs.positional("pid", {
+            describe: "The PID to monitor",
+            type: "number"
+        })
+    })
+    .demandCommand(1)
+    .version('v0.1.0')
+    .help()
+    .parseSync()
+
+console.dir(argv._)
+pidusage(argv._[0], { "usePs": true }, (err, stats: pidusage.Status) => {
     if (err) {
         console.warn("Error:", err.message)
         return
