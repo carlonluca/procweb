1efb91053c0f0b7e019892e90d400794f48ce0a0
Luca Carlon <carlon.luca@gmail.com>
2022-09-24 01:39:58 +0200

    Draft of chart with data populated from the server.

--- procweb-webui/src/app/app.component.ts
+++ procweb-webui/src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, min, retry } from 'rxjs/operators';
+import { EChartsOption } from 'echarts';
+
+interface Sample {
+  timestamp: number,
+  value: number
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'procweb-webui';
+  constructor(private http: HttpClient) { }
+  title = 'procweb-webui'
+  echartData: number[][] = []
+  chartOption: EChartsOption = {
+    xAxis: {
+      type: 'value',
+      min: (new Date()).getTime() - 1000000,
+      max: (new Date()).getTime() + 100000
+    },
+    yAxis: {
+      type: 'value',
+      min: 0,
+      max: 100
+    },
+    series: [
+      {
+        type: "line",
+        data: this.echartData
+      }
+    ]
+  }
+
+  ngOnInit() {
+    this.getSamples().subscribe((data: Sample[]) => {
+      data.forEach((sample: Sample) => {
+        this.echartData.push([ sample.timestamp, sample.value ])
+      })
+
+    })
+  }
+
+  getSamples() {
+    return this.http.get<Sample[]>("/api/samples/cpu")
+  }
 }
--- procweb-webui/src/app/app.module.ts
+++ procweb-webui/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxEchartsModule.forRoot({
+      /**
+       * This will import all modules from echarts.
+       * If you only need custom modules,
+       * please refer to [Custom Build] section.
+       */
+      echarts: () => import('echarts'), // or import('./path-to-my-custom-echarts')
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
