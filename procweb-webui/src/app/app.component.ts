bda582ba4bbf678607e8eb58410f9d5ca508d795
Luca Carlon <carlon.luca@gmail.com>
2022-09-24 12:52:52 +0200

    Update chart.

--- procweb-webui/src/app/app.component.ts
+++ procweb-webui/src/app/app.component.ts
@@ -4,79 +4,95 @@ import { EChartsOption } from 'echarts';
 import { XAXisComponentOption } from "echarts"
 
 interface Sample {
-  timestamp: number,
-  value: number
+    timestamp: number,
+    value: number
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private http: HttpClient) { }
-  title = 'procweb-webui'
-  echartData: number[][] = []
-  chartOption: EChartsOption = {
-    xAxis: {
-      type: 'value',
-      min: 0,
-      max: 0
-    },
-    yAxis: {
-      type: 'value',
-      min: 0,
-      max: 100
-    },
-    series: [
-      {
-        type: "line",
-        data: this.echartData
-      }
-    ]
-  }
+    constructor(private http: HttpClient) { }
+    title = 'procweb-webui'
+    echartData: number[][] = []
+    chartOption: EChartsOption = {
+        xAxis: {
+            type: 'value',
+            min: 0,
+            max: 0,
+            axisLabel: {
+                rotate: 45,
+                formatter: (value: number, index: number): string => {
+                    const options: Intl.DateTimeFormatOptions = {
+                        weekday: undefined,
+                        year: undefined,
+                        month: 'numeric',
+                        day: 'numeric',
+                        hour: "numeric",
+                        minute: "numeric",
+                        second: "numeric",
+                        hour12: false
+                    }
+                    return new Date(value).toLocaleString(undefined, options)
+                }
+            }
+        },
+        yAxis: {
+            type: 'value',
+            min: 0,
+            max: 100
+        },
+        series: [
+            {
+                type: "line",
+                data: this.echartData
+            }
+        ]
+    }
 
-  ngOnInit() {
-    this.getSamples().subscribe((data: Sample[]) => {
-      data.forEach((sample: Sample) => {
-        this.echartData.push([sample.timestamp, sample.value])
-      })
-      let xMin: number = this.arrayMinTimestamp(data);
-      let xMax: number = this.arrayMaxTimestamp(data);
-      let yMin: number = this.arrayMinValue(data);
-      let yMax: number = this.arrayMaxValue(data);
-      (this.chartOption.xAxis! as XAXisComponentOption).min = xMin;
-      (this.chartOption.xAxis! as XAXisComponentOption).max = xMax;
-      (this.chartOption.yAxis! as XAXisComponentOption).min = yMin;
-      (this.chartOption.yAxis! as XAXisComponentOption).max = yMax;
-    })
-  }
+    ngOnInit() {
+        this.getSamples().subscribe((data: Sample[]) => {
+            data.forEach((sample: Sample) => {
+                this.echartData.push([sample.timestamp, sample.value])
+            })
+            let xMin: number = this.arrayMinTimestamp(data);
+            let xMax: number = this.arrayMaxTimestamp(data);
+            let yMin: number = this.arrayMinValue(data);
+            let yMax: number = this.arrayMaxValue(data);
+            (this.chartOption.xAxis! as XAXisComponentOption).min = xMin;
+            (this.chartOption.xAxis! as XAXisComponentOption).max = xMax;
+            (this.chartOption.yAxis! as XAXisComponentOption).min = yMin;
+            (this.chartOption.yAxis! as XAXisComponentOption).max = yMax;
+        })
+    }
 
-  getSamples() {
-    return this.http.get<Sample[]>("/api/samples/cpu")
-  }
+    getSamples() {
+        return this.http.get<Sample[]>("/api/samples/cpu")
+    }
 
-  arrayMinTimestamp(arr: Sample[]): number {
-    return arr.reduce((p: Sample, v: Sample) => {
-      return p.timestamp < v.timestamp ? p : v
-    }).timestamp
-  }
+    arrayMinTimestamp(arr: Sample[]): number {
+        return arr.reduce((p: Sample, v: Sample) => {
+            return p.timestamp < v.timestamp ? p : v
+        }).timestamp
+    }
 
-  arrayMaxTimestamp(arr: Sample[]): number {
-    return arr.reduce((p, v): Sample => {
-      return p.timestamp > v.timestamp ? p : v
-    }).timestamp
-  }
+    arrayMaxTimestamp(arr: Sample[]): number {
+        return arr.reduce((p, v): Sample => {
+            return p.timestamp > v.timestamp ? p : v
+        }).timestamp
+    }
 
-  arrayMinValue(arr: Sample[]): number {
-    return arr.reduce((p, v): Sample => {
-      return p.value < v.value ? p : v
-    }).value
-  }
+    arrayMinValue(arr: Sample[]): number {
+        return arr.reduce((p, v): Sample => {
+            return p.value < v.value ? p : v
+        }).value
+    }
 
-  arrayMaxValue(arr: Sample[]): number {
-    return arr.reduce((p, v): Sample => {
-      return p.value > v.value ? p : v
-    }).value
-  }
+    arrayMaxValue(arr: Sample[]): number {
+        return arr.reduce((p, v): Sample => {
+            return p.value > v.value ? p : v
+        }).value
+    }
 }
--- procweb-webui/src/app/app.module.ts
+++ procweb-webui/src/app/app.module.ts
@@ -6,23 +6,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    NgxEchartsModule.forRoot({
-      /**
-       * This will import all modules from echarts.
-       * If you only need custom modules,
-       * please refer to [Custom Build] section.
-       */
-      echarts: () => import('echarts'), // or import('./path-to-my-custom-echarts')
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      NgxEchartsModule.forRoot({
+         /**
+          * This will import all modules from echarts.
+          * If you only need custom modules,
+          * please refer to [Custom Build] section.
+          */
+         echarts: () => import('echarts'), // or import('./path-to-my-custom-echarts')
+      })
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
