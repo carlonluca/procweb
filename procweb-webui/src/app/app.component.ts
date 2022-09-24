aea359ccc2b361bab79c59c285b0d00613919c5d
Luca Carlon <carlon.luca@gmail.com>
2022-09-24 02:48:21 +0200

    Improve the chart range.

--- procweb-webui/src/app/app.component.ts
+++ procweb-webui/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, min, retry } from 'rxjs/operators';
 import { EChartsOption } from 'echarts';
+import { XAXisComponentOption } from "echarts"
 
 interface Sample {
   timestamp: number,
@@ -21,8 +20,8 @@ export class AppComponent {
   chartOption: EChartsOption = {
     xAxis: {
       type: 'value',
-      min: (new Date()).getTime() - 1000000,
-      max: (new Date()).getTime() + 100000
+      min: 0,
+      max: 0
     },
     yAxis: {
       type: 'value',
@@ -40,13 +39,44 @@ export class AppComponent {
   ngOnInit() {
     this.getSamples().subscribe((data: Sample[]) => {
       data.forEach((sample: Sample) => {
-        this.echartData.push([ sample.timestamp, sample.value ])
+        this.echartData.push([sample.timestamp, sample.value])
       })
-
+      let xMin: number = this.arrayMinTimestamp(data);
+      let xMax: number = this.arrayMaxTimestamp(data);
+      let yMin: number = this.arrayMinValue(data);
+      let yMax: number = this.arrayMaxValue(data);
+      (this.chartOption.xAxis! as XAXisComponentOption).min = xMin;
+      (this.chartOption.xAxis! as XAXisComponentOption).max = xMax;
+      (this.chartOption.yAxis! as XAXisComponentOption).min = yMin;
+      (this.chartOption.yAxis! as XAXisComponentOption).max = yMax;
     })
   }
 
   getSamples() {
     return this.http.get<Sample[]>("/api/samples/cpu")
+  }
+
+  arrayMinTimestamp(arr: Sample[]): number {
+    return arr.reduce((p: Sample, v: Sample) => {
+      return p.timestamp < v.timestamp ? p : v
+    }).timestamp
+  }
+
+  arrayMaxTimestamp(arr: Sample[]): number {
+    return arr.reduce((p, v): Sample => {
+      return p.timestamp > v.timestamp ? p : v
+    }).timestamp
+  }
+
+  arrayMinValue(arr: Sample[]): number {
+    return arr.reduce((p, v): Sample => {
+      return p.value < v.value ? p : v
+    }).value
+  }
+
+  arrayMaxValue(arr: Sample[]): number {
+    return arr.reduce((p, v): Sample => {
+      return p.value > v.value ? p : v
+    }).value
   }
 }
