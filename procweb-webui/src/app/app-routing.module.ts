0ca4d366cd3303e09feed70133507b98355ef1be
Luca Carlon <carlon.luca@gmail.com>
2022-12-13 21:33:42 +0100

    Added license to the source files.

--- procweb-webui/src/app/app-routing.module.ts
+++ procweb-webui/src/app/app-routing.module.ts
@@ -1,3 +1,27 @@
+/*
+ * This file is part of procweb.
+ *
+ * Copyright (c) 2022 Luca Carlon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Author:  Luca Carlon
+ * Date:    2022.12.13
+ * Company: -
+ */
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
--- procweb-webui/src/app/samples.service.spec.ts
+++ procweb-webui/src/app/samples.service.spec.ts
@@ -1,5 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+/*
+ * This file is part of procweb.
+ *
+ * Copyright (c) 2022 Luca Carlon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
+/**
+ * Author:  Luca Carlon
+ * Date:    2022.12.13
+ * Company: -
+ */
+
+import { TestBed } from '@angular/core/testing';
 import { SamplesService } from './samples.service';
 
 describe('SamplesService', () => {
