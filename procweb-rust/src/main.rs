e8a543b562d888cde56ee9664f96744d0a8c7385
Luca Carlon <carlon.luca@gmail.com>
2023-02-15 19:17:24 +0100

    Implemented acquisition of cpu samples.

--- procweb-rust/src/main.rs
+++ procweb-rust/src/main.rs
@@ -2,13 +2,12 @@ use std::sync::Arc;
 use std::sync::Mutex;
 
 use actix_web::{get, web, App, HttpServer, Responder};
-use log::{debug, error, log_enabled, info, Level};
 use pwsampler::PWSampler;
 mod pwsampler;
 mod pwdata;
 
 #[get("/api/samples")]
-async fn getSamples(data: web::Data<Arc<Mutex<PWSampler>>>) -> impl Responder {
+async fn get_samples(data: web::Data<Arc<Mutex<PWSampler>>>) -> impl Responder {
     let samples = data.lock().unwrap().samples();
     let _samples = samples.lock().unwrap();
     let __samples = &*_samples;
@@ -29,7 +28,9 @@ async fn main() -> std::io::Result<()> {
     sampler.lock().unwrap().start();
 
     HttpServer::new(move || {
-        App::new().app_data(sampler.clone()).service(getSamples)
+        App::new()
+            .app_data(web::Data::new(sampler.clone()))
+            .service(get_samples)
     })
     .bind(("127.0.0.1", 3000))?
     .run()
--- procweb-rust/src/pwsampler.rs
+++ procweb-rust/src/pwsampler.rs
@@ -1,8 +1,10 @@
 use std::thread;
 use std::thread::sleep;
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::sync::{Arc, Mutex};
-use log::{debug, error, log_enabled, info, Level};
+use std::rc::Rc;
+use sysinfo::{System, SystemExt, CpuExt};
+use log;
 use crate::pwdata::PWSample;
 extern crate timer;
 extern crate chrono;
@@ -27,11 +29,14 @@ impl PWSampler {
         let samples = self.samples.clone();
         self.thread_handle = Some(thread::spawn(move || {
             // some work here
+            let mut sys = System::new_all();
             loop {
-                let mut data = samples.lock().unwrap();
-                (*data).push(PWSampler::acquire_sample());
+                {
+                    let mut data = samples.lock().unwrap();
+                    (*data).push(PWSampler::acquire_sample(&mut sys));
+                }
+                
                 sleep(Duration::from_secs(1));
-                log::info!("Sample: {:?}", (*data).last());
             }
         }));
     }
@@ -42,7 +47,29 @@ impl PWSampler {
 
     // Private portion
     // ===============
-    fn acquire_sample() -> PWSample {
-        return PWSample::default();
+    fn acquire_sample(sys: &mut System) -> PWSample {
+        let now = SystemTime::now();
+        let ts = match now.duration_since(UNIX_EPOCH) {
+            Ok(d) => d,
+            Err(err) => {
+                log::warn!("Could not get time: {:?}", err);
+                Duration::from_millis(0)
+            }
+        };
+
+        sys.refresh_all();
+
+        let mut avg_cpu = 0f64;
+        for cpu in sys.cpus() {
+            avg_cpu += cpu.cpu_usage() as f64;
+        }
+        avg_cpu = avg_cpu/sys.cpus().len() as f64;
+
+        let mut def = PWSample::default();
+        def.ts = ts.as_millis() as i64;
+        def.cpu = avg_cpu;
+
+        log::info!("Sample: {:?}", def);
+        def
     }
 }
